ff6edd9fc74ddc469f42bd252df48efad00a3ebe
Mritunjay <151187227+Maman08@users.noreply.github.com>
2024-12-22 13:09:45 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,3 @@
-// import { useState,useEffect } from 'react'
-// import abi from "./contractJson/Medicalsys.json"
-// import './App.css'
-// import { providers } from 'ethers'
-// import { ethers } from 'ethers'
-
-// function App() {
-// const [state,setState]=useState({provider:null,signer:null,contract:null});
-// const [account,setAccount]=useState("Not Connected");
-// useEffect(()=>{
-//   const template=async()=>{
-//     const contractAddress="0x4d3Daf9b7876befF2D3b3A5400C6fD9AA15943D0";
-//     const contractABI=abi.abi;
-//     try{
-//       const {ethereum}=window;
-//       const account = await ethereum.request({
-//         method:"eth_requestAccounts"
-//       })
-//       setAccount(account);
-//       const provider=new ethers.Web3Provider(ethereum);
-//       const signer=provider.getSigner();
-//       const contract=new ethers.Contract(
-//         contractAddress,contractABI,signer
-//       )
-//       console.log(contract);
-//       setState({provider,signer,contract})
-//     }catch(error){
-//       alert(error);
-//     }
-    
-//   }
-//   template();
-// },[])
-//   return (
-//     <div className='App'>
-
-//     </div>
-//   )
-// }
-
-// export default App
 
 
 import React, { useState, useEffect } from 'react';
@@ -89,7 +48,7 @@ function App() {
 
 
   const appStyle = {
-    backgroundImage: `url(${bgImg})`, // Replace with your image path
+    backgroundImage: `url(${bgImg})`, 
    
     backgroundAttachment: 'fixed'
   };
@@ -97,7 +56,6 @@ function App() {
     
     <div className='App' style={appStyle}>
       <h2>Connected Account: {account}</h2>
-      {/* Add your contract interaction UI here */}
       <Manufacturer state={state}></Manufacturer>
        <Memos state={state}></Memos> 
        <VerifyMedicine/>
